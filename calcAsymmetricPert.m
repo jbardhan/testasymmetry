2c1ca3dfe2e274f746a173ba019c7256836a1551
Jay Bardhan <jbardhan@alum.mit.edu>
2014-07-30 17:59:43 -0400

    Added self-consistent solve -- neglects singularity due to going through charge layer (see comment on line 9 of calcAsymmetricPert)

--- calcAsymmetricPert.m
+++ calcAsymmetricPert.m
@@ -1,11 +1,11 @@
 function deltaA = calcAsymmetricPert(asymParams, surfdata, surfsurfop, ...
-				     chargesurfop, pqr)
+				     chargesurfop, pqr, sigma)
 
 alpha = asymParams.alpha;
 beta  = asymParams.beta;
 EfieldOffset = asymParams.EfieldOffset;
 deltaOffset  = asymParams.deltaOffset;
 
-Efield = -chargesurfop.dphidnCoul * pqr.q;
+Efield = -chargesurfop.dphidnCoul * pqr.q - surfsurfop.K'*sigma;% - 0.5*sigma;
 deltaA = diag(surfdata.weights.*(alpha*(tanh(beta*Efield-EfieldOffset)) ...
 				 +deltaOffset));
--- solveAsymmetric.m
+++ solveAsymmetric.m
@@ -5,9 +5,10 @@ function [phiReac, sigma] = solveAsymmetric(surfdata, surfsurfop, ...
 % this is the simpler, not self-consistent version (self-consistency
 % means adding bem.A*sigma to asymPert and looping)
 maxIter = 100;
+sigma = zeros(size(bem.A,1),1);
 rhs = bem.B * pqr.q;
 asymPert = calcAsymmetricPert(asymParams, surfdata, surfsurfop, ...
-			      chargesurfop, pqr);
+			      chargesurfop, pqr, sigma);
 [sigma,flag,relres,iter,resvec] = gmres(bem.A+asymPert, rhs, [], 1e-5, maxIter);
 phiReac = convFactor * bem.C * sigma;
 
