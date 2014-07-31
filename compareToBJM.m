6336ea068d4d698b3ae03df42fb8220c67937fb7
Jay Bardhan <jbardhan@alum.mit.edu>
2014-07-31 01:49:44 -0400

    no message

--- compareToBJM.m
+++ compareToBJM.m
@@ -3,14 +3,48 @@ printOn = 0;
 addpath('../pointbem');
 loadConstants
 asymParams = struct('alpha', 0.6, 'beta', -20.0,'EfieldOffset',0,'deltaOffset',0);
-
-origin = [0 0 0];
-R      = 5.4;
 epsIn  =  1;
 epsOut = 80;
-numCharges = -1;
 conv_factor = 332.112;
+
+curdir = pwd;
+%%%%%%%%%%%% Needs to be replaced with actual data
+cd ~/research/static/newsphere/offsetsFine_sph4
+cd ../sph4
+origin
+cd ../offsetsFine_sph4
+
+xOrigin = localfit(Qdata, Edata);
+off00 = struct('Qdata',Qdata,'Edata',Edata);
+
+loadOff05
+x05 = localfit(Qdata, Edata);
+newLoadOff10
+x10 = localfit(Qdata, Edata);
+newLoadOff15
+x15 = localfit(Qdata, Edata);
+newLoadOff20
+x20 = localfit(Qdata, Edata);
+newLoadOff25
+x25 = localfit(Qdata, Edata);
+fixLoad30 %%%%%%% 
+x30 = localfit(Qdata, Edata);
+newLoadOff35
+x35 = localfit(Qdata, Edata);
+%%%%%%%%%%%%%%
+cd(curdir);
+[junk,Iall]=sort(off00.Qdata);
 staticPotential =  43.5/4.18;  % from Bardhan12_Jungwirth_Makowski
+E_at_m1 = mean(off00.Edata(Iall(1:2))) - (-1.0)*staticPotential;
+R_m1 = .5 * conv_factor * (1/epsOut - 1/epsIn) /E_at_m1;
+E_at_p1 = mean(off00.Edata(Iall(200:201))) - (1.0) * staticPotential;
+R_p1 = .5 * conv_factor * (1/epsOut - 1/epsIn) /E_at_p1;
+
+origin = [0 0 0];
+R      = R_p1;  % or substitute with R_m1.  results don't change
+                % substantially
+		
+numCharges = -1;
 waterModel = struct('tau',1,'R_OH',0.58,'rho_w',1.4,'R_s',0.52); 
 % from Mukhopadhyay
 
@@ -19,6 +53,7 @@ density = 2.0;
 numPoints = ceil(4 * pi * density * R^2)
 surfdata   = makeSphereSurface(origin, R, numPoints);
 surfsurfop = makeSurfaceToSurfaceOperators(surfdata);
+clear origin
 
 h = 0.5;
 h2 = 1.5;
@@ -26,7 +61,7 @@ lineCharges = [0 2 2.5 3 3.5];%lineCharges = 0.0:h:R-h2;
 plusOne = 1.0;
 minusOne = -1.0;
 
-q_list = linspace(-1,1,20);
+q_list = linspace(-1,1,40);
 for i=1:length(lineCharges)
   for j=1:length(q_list)
     q = q_list(j);
@@ -68,17 +103,34 @@ for i=1:length(lineCharges)
   end
 end
 
-
 figure; set(gca,'fontsize',16)
-plot(q_list,dG_bem(1,:),'ro-','linewidth',2,'markersize',10)
-hold
-plot(q_list,dG_bem(2,:),'g^-','linewidth',2,'markersize',10)
-plot(q_list,dG_bem(3,:),'k>-','linewidth',2,'markersize',10)
-plot(q_list,dG_bem(4,:),'b<-','linewidth',2,'markersize',10)
-plot(q_list,dG_bem(5,:),'ms-','linewidth',2,'markersize',10)
+[junk,Iall] = sort(off00.Qdata);
+plot(off00.Qdata(Iall),off00.Edata(Iall),'ro','markersize',8,'linewidth',1.5);
+hold on;
+
+[junk,Iall] = sort(off20.Qdata);
+plot(off20.Qdata(Iall),off20.Edata(Iall),'g^','markersize',8,'linewidth',1.5);
+
+[junk,Iall] = sort(off25.Qdata);
+plot(off25.Qdata(Iall),off25.Edata(Iall),'k>','markersize',8,'linewidth',1.5);
+
+[junk,Iall] = sort(off30.Qdata);
+plot(off30.Qdata(Iall),off30.Edata(Iall),'b<','markersize',8,'linewidth',1.5);
+
+[junk,Iall] = sort(off35.Qdata);
+plot(off35.Qdata(Iall),off35.Edata(Iall),'ms','markersize',8,'linewidth',1.5);
+
+plot(q_list,dG_bem(1,:),'r-','linewidth',2,'markersize',10)
+plot(q_list,dG_bem(2,:),'g--','linewidth',2,'markersize',10)
+plot(q_list,dG_bem(3,:),'k-.','linewidth',2,'markersize',10)
+plot(q_list,dG_bem(4,:),'b:','linewidth',2,'markersize',10)
+plot(q_list,dG_bem(5,:),'m-.','linewidth',2,'markersize',10)
+
 xlabel('Charge q');
 ylabel('\Delta G^{charging} (kcal/mol)');
-legend('0.0','2.0','2.5','3.0','3.5','location','southeast');
+legend('FEP 0.0','FEP 2.0','FEP 2.5','FEP 3.0','FEP 3.5',...
+       'NLBC 0.0', 'NLBC 2.0', 'NLBC 2.5', 'NLBC 3.0', 'NLBC 3.5',...
+       'location','south');
 
 return
 
@@ -107,6 +159,4 @@ legend('\Delta G_{GB} q=+1', '\Delta G_{BEM} q=+1', ...
        '\Delta G_{GB} q=-1', ...
        '\Delta G_{BEM} q=-1',...
        'location','southwest');
-
-
 
--- testIonRadiusFunction.m
+++ testIonRadiusFunction.m
@@ -1,6 +1,8 @@
+printOn = 1;
+
 addpath('../pointbem');
 loadConstants
-as = [0.6 -16 0 0];
+asymParams = struct('alpha', 0.6, 'beta', -20.0,'EfieldOffset',0,'deltaOffset',0);
 
 origin = [0 0 0];
 R_list = linspace(1,4,30);
@@ -29,21 +31,44 @@ for i=1:length(R_list)
     bem = makeBemMatrices(surfdata, pqr, surfsurfop, ...
 			  chargesurfop,  epsIn, epsOut);
 
-    rhs = bem.B*pqr.q;
-    
-    Efield = -chargesurfop.dphidnCoul * pqr.q;
-    diagPert = diag(surfdata.weights.*(as(1)*(tanh(as(2)*Efield-as(3)))+as(4)));
-    newBem = bem.A + diagPert;
-    sigma = gmres(newBem, rhs, [], 1e-5, min(100,size(bem.A,1)));
-    
-    L(i,j) = kcal_to_kJ* conv_factor * (0.5 * q * bem.C * sigma);
-    
-  end    
+    [phiReac, sigma] = solveConsistentAsymmetric(surfdata, surfsurfop, ...
+						 chargesurfop, bem, ...
+						 epsIn, epsOut, ...
+						 conv_factor, pqr, asymParams);
+    L(i,j) = 0.5 * q'*phiReac;
+  end 
+  E_born(i) = 0.5 * conv_factor * (1/epsOut - 1/epsIn)/R;
 end
   
 figure; set(gca,'fontsize',16);
-plot(R_list, L(:,1),'r','linewidth',2); hold on;
-plot(R_list, L(:,end),'b--','linewidth',2);
-legend('q = -1', 'q = +1');
-xlabel('R_{ion}');
-ylabel('Charging free energy (kJ/mol)');
+plot(R_list, L(:,1),'r-','linewidth',2,'markersize',10); hold on;
+plot(R_list, L(:,end),'b-.','linewidth',2,'markersize',10);
+plot(R_list, E_born,'k--','linewidth',1.5, 'markersize',10);
+legend('q = -1', 'q = +1','location','southeast');
+xlabel('R_{ion} (Angstrom)');
+ylabel('Charging free energy (kcal/mol)');
+
+% from Bardhan12_Jungwirth_Makowski
+sodiumRminOver2 = 1.36375;
+sodiumStatic = 11.16;
+sodiumPlus = -93.4 - (1.0) * sodiumStatic; 
+sodiumMinus = -175.7 - (-1.0) * sodiumStatic; 
+
+chlorideRminOver2 = 2.27;
+chlorideStatic = 9.15;
+chloridePlus = -57.0 - (1.0) * chlorideStatic;
+chlorideMinus = -95.3 - (-1.0) * chlorideStatic;
+
+deltaR = 0.15;
+plot([sodiumRminOver2 sodiumRminOver2], [sodiumMinus sodiumPlus], 'ks','markersize',8,'linewidth',2);
+%plot([sodiumRminOver2-deltaR sodiumRminOver2+deltaR],[sodiumMinus sodiumMinus],'k-.');
+%plot([sodiumRminOver2-deltaR sodiumRminOver2+deltaR],[sodiumPlus sodiumPlus],'k-.');
+
+plot([chlorideRminOver2 chlorideRminOver2], [chlorideMinus chloridePlus], 'ks','markersize',8,'linewidth',2);
+%plot([chlorideRminOver2-deltaR chlorideRminOver2+deltaR],[chlorideMinus chlorideMinus],'k-.');
+%plot([chlorideRminOver2-deltaR chlorideRminOver2+deltaR],[chloridePlus chloridePlus],'k-.');
+
+if printOn 
+  print -depsc2 vary-ion-radius.eps
+  print -dpng   vary-ion-radius.png
+end
