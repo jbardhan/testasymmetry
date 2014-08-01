b94f164ea18983b3029b331dac0508b093d5ea43
Jay Bardhan <jbardhan@alum.mit.edu>
2014-08-01 02:45:21 -0400

    Fixed a bug in computing deltaOffset

--- calcAsymmetricPert.m
+++ calcAsymmetricPert.m
@@ -4,7 +4,7 @@ function deltaA = calcAsymmetricPert(asymParams, surfdata, surfsurfop, ...
 alpha = asymParams.alpha;
 beta  = asymParams.beta;
 EfieldOffset = asymParams.EfieldOffset;
-deltaOffset  = asymParams.deltaOffset;
+deltaOffset  = -alpha * tanh(beta*0-EfieldOffset);
 
 Efield = -chargesurfop.dphidnCoul * pqr.q - surfsurfop.K'*sigma;% - 0.5*sigma;
 deltaA = diag(surfdata.weights.*(alpha*(tanh(beta*Efield-EfieldOffset)) ...
