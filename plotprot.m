88bc9b3c3af63b7eb8951728cdbd5e98a1425a59
Jay Bardhan <jbardhan@alum.mit.edu>
2014-08-14 16:36:41 -0500

    Fixed some plotting for the residues case, and adding PARSE radii… this should be fun.

--- plotprot.m
+++ plotprot.m
@@ -7,6 +7,11 @@ printOn = 0;
 % the above webpage says that the OD* and OE* radii for ASP and GLU
 % should be 1.40 not 1.42, as reported in the Nina97 pub
 
+% the "natural order" was alphabetical by amino acid.
+% for plot clarity we may want to change that with an index set,
+% but this'll be a hassle if we also want to plot the Nina et al results.
+indexSet = 1:7; 
+
 rouxProt = [1 3 5 6 7];
 rouxProtEnergies = [-66.0 -15.5 -68.15 -72.9 -19.0];
 rouxProtPoisson  = [-67.0 -15.4 -68.10 -72.2 -19.1]; 
@@ -20,6 +25,7 @@ bkProt = [1 2 3 4 5 6 7];
 bkProtEnergies = [(-54.44-correction) % JR1 (prot=charged)
 		  -12.57 % JD1 (prot = neutral)
 		  -12.82 % JC1 (prot = neutral)
+		  0 % JE1 (prot = neutral) no answer yet
 		  (-49.95-correction)  %JH1 (prot=charged)
 		  -72.9 % JK1 from Roux!! (prot=neutral)
 		  -17.6]; % JY1, from nsteps=2k,needs 8k redone
@@ -27,24 +33,29 @@ bkDeprot = [1 2 3 4 5 6 7];
 bkDeprotEnergies = [-29.84 % JR2, deprot = neutral
 		    (-82.65-correction) % JD2, deprot = charged
 		    (-84.46-correction) % JC2, deprot = charged
-		    (-84.25-correction) % JE2, deprot = charged
+		    (-85.02-correction) % JE2, deprot = charged
 		    -20.75 % JH2, deprot = neutral, needs final answer
 		    -15.36  % JK2, deprot = neutral
 		    (-103.69-correction)]; % JY2, deprot = charged,
                                            % needs to be redone
 
 figure; set(gca,'fontsize',16);
-plot(rouxProt,rouxProtEnergies,'ks','linewidth',2,'markersize',12);
+plot(bkProt,bkProtEnergies,'ks','linewidth',4,'markersize',12);
 hold on;
-plot(rouxDeprot,rouxDeprotEnergies,'ks','linewidth',2,'markersize',12);
-plot(bkProt,bkProtEnergies,'md','linewidth',4,'markersize',12);
-plot(bkDeprot,bkDeprotEnergies,'md','linewidth',4,'markersize',12);
+plot(bkDeprot,bkDeprotEnergies,'ks','linewidth',4,'markersize',12);
 
-plot(asym(:,1),'bx','linewidth',2,'markersize',10);
-plot(asym(:,2),'rx','linewidth',2,'markersize',10);
+plot(bkProt, asym(:,1),'bo','linewidth',4,'markersize',10);
+plot(bkDeprot, asym(:,2),'bo','linewidth',4,'markersize',10);
 % 
-plot(sym(:,1),'b+','linewidth',1,'markersize',10);
-plot(sym(:,2),'r+','linewidth',1,'markersize',10);
+plot(bkProt,sym(:,1),'mx','linewidth',2,'markersize',12);
+plot(bkDeprot, sym(:,2),'mx','linewidth',2,'markersize',12);
 
-plot(rouxProt,rouxProtEnergies,'ks','linewidth',2,'markersize',6);
-plot(rouxDeprot,rouxDeprotEnergies,'ks','linewidth',2,'markersize',6);
+plot(rouxProt,rouxProtEnergies,'rs','linewidth',2,'markersize',8);
+plot(rouxDeprot,rouxDeprotEnergies,'rs','linewidth',2,'markersize',8);
+
+ylabel('Charging free enegy (kcal/mol)')
+
+if printOn 
+  print -depsc2 residues.eps
+  print -dpng residues.png
+end
--- residues/changeRadii.sh
+++ residues/changeRadii.sh
@@ -2,36 +2,36 @@
 
 cd arg
 #~/repos/fftsvd/meshmaker arg.pdb ../scaled_charmm22.siz arg_scaledcharmm_6.srf 1.4 2.0 6 1 1 0 .
-~/repos/fftsvd/meshmaker arg.pdb ../roux_ses.siz arg_roux_6.srf 1.4 2.0 6 1 1 0 .
+~/repos/fftsvd/meshmaker arg.pdb ../radii_fixedNH3_parse.siz arg_parse_6.srf 1.4 2.0 6 1 1 0 .
 cd ..
 
 cd asp
 #~/repos/fftsvd/meshmaker asp.pdb ../scaled_charmm22.siz asp_scaledcharmm_6.srf 1.4 2.0 6 1 1 0 .
-~/repos/fftsvd/meshmaker asp.pdb ../roux_ses.siz asp_roux_6.srf 1.4 2.0 6 1 1 0 .
+~/repos/fftsvd/meshmaker asp.pdb ../radii_fixedNH3_parse.siz asp_parse_6.srf 1.4 2.0 6 1 1 0 .
 cd ..
 
 cd cys
 #~/repos/fftsvd/meshmaker cys.pdb ../scaled_charmm22.siz cys_scaledcharmm_6.srf 1.4 2.0 6 1 1 0 .
-~/repos/fftsvd/meshmaker cys.pdb ../roux_ses.siz cys_roux_6.srf 1.4 2.0 6 1 1 0 .
+~/repos/fftsvd/meshmaker cys.pdb ../radii_fixedNH3_parse.siz cys_parse_6.srf 1.4 2.0 6 1 1 0 .
 cd ..
 
 cd glu
 #~/repos/fftsvd/meshmaker glu.pdb ../scaled_charmm22.siz glu_scaledcharmm_6.srf 1.4 2.0 6 1 1 0 .
-~/repos/fftsvd/meshmaker glu.pdb ../roux_ses.siz glu_roux_6.srf 1.4 2.0 6 1 1 0 .
+~/repos/fftsvd/meshmaker glu.pdb ../radii_fixedNH3_parse.siz glu_parse_6.srf 1.4 2.0 6 1 1 0 .
 cd ..
 
 cd his
 #~/repos/fftsvd/meshmaker his.pdb ../scaled_charmm22.siz his_scaledcharmm_6.srf 1.4 2.0 6 1 1 0 .
-~/repos/fftsvd/meshmaker his.pdb ../roux_ses.siz his_roux_6.srf 1.4 2.0 6 1 1 0 .
+~/repos/fftsvd/meshmaker his.pdb ../radii_fixedNH3_parse.siz his_parse_6.srf 1.4 2.0 6 1 1 0 .
 cd ..
 
 cd lys
 #~/repos/fftsvd/meshmaker lys.pdb ../scaled_charmm22.siz lys_scaledcharmm_6.srf 1.4 2.0 6 1 1 0 .
-~/repos/fftsvd/meshmaker lys.pdb ../roux_ses.siz lys_roux_6.srf 1.4 2.0 6 1 1 0 .
+~/repos/fftsvd/meshmaker lys.pdb ../radii_fixedNH3_parse.siz lys_parse_6.srf 1.4 2.0 6 1 1 0 .
 cd ..
 
 cd tyr
 #~/repos/fftsvd/meshmaker tyr.pdb ../scaled_charmm22.siz tyr_scaledcharmm_6.srf 1.4 2.0 6 1 1 0 .
-~/repos/fftsvd/meshmaker tyr.pdb ../roux_ses.siz tyr_roux_6.srf 1.4 2.0 6 1 1 0 .
+~/repos/fftsvd/meshmaker tyr.pdb ../radii_fixedNH3_parse.siz tyr_parse_6.srf 1.4 2.0 6 1 1 0 .
 cd ..
 
