58bd4785f55ca7cad8292173009082f91cf6911e
Jaydeep Bardhan <jbardhan@alum.mit.edu>
2014-10-03 14:50:41 -0400

    Changed L names for ecf formulation means we need to change our plot variables.  Added first step towards BornAnalytical test script

--- testBornAnal.m
+++ testBornAnal.m
@@ -1,13 +1,10 @@
-printOn = 1;
-
 addpath('../pointbem');
 loadConstants
-% alpha = 0.3, beta = -60, EfieldOffset = 0 works well for the ions
-asymParams = struct('alpha',0.0, 'beta', -60.0,'EfieldOffset',-0.5);
+asymParams = struct('alpha',0.5, 'beta', -60.0,'EfieldOffset',-0.5);
 
 origin = [0 0 0];
 R_list = linspace(1,2.5,5);
-q_list = [-1 1]; %linspace(-1,1,20);
+q_list = [-1 1]; 
 
 epsIn  =  1;
 epsOut = 80;
@@ -16,7 +13,7 @@ conv_factor = 332.112;
 kcal_to_kJ = 4.18;
 waterModel = struct('tau',1,'R_OH',0.58,'rho_w',1.4,'R_s',0.52); 
 staticPotential = 10.7;
-density = 4.0;
+density = 12.0;
 
 
 for i=1:length(R_list)
@@ -32,7 +29,7 @@ for i=1:length(R_list)
     [phiReacEcf, sigma] = solveConsistentAsymmetric(surfdata, bemEcf, ...
 						 epsIn, epsOut, ...
 						 conv_factor, pqr, asymParams);
-    [phiReacYoonDiel, phiBndy,dPhiBndy] = solveConsistentYoonDielAsym(surfdata, bemYoonDiel, ...
+    [phiReacYoonDiel, phiBndy,dPhiBndy] = solveConsistentYoonNoSternAsym(surfdata, bemYoonDiel, ...
 						 epsIn, epsOut, ...
 						 conv_factor, pqr, asymParams);
     L_ecf(i,j) = 0.5 * q'*phiReacEcf;
