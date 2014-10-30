03e3f944cca488b2ded66587a558d591660cbd52
Jay Bardhan <jbardhan@alum.mit.edu>
2014-10-30 18:51:36 -0400

    Added a folder with different meshes for different radii, with and without Stern layer

---
+++ saltresidues/changeRadii.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+cd arg
+~/repos/fftsvd/meshmaker arg.pdb ../scaled_charmm22.siz arg_scaledcharmm_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker arg.pdb ../scaled_charmm22.siz arg_scaledcharmm_stern_5.srf 1.4 2.0 5 1 1 1 .
+~/repos/fftsvd/meshmaker arg.pdb ../radii_fixedNH3_parse.siz arg_parse_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker arg.pdb ../radii_fixedNH3_parse.siz arg_parse_stern_5.srf 1.4 2.0 5 1 1 1 .
+~/repos/fftsvd/meshmaker arg.pdb ../roux_ses.siz arg_roux_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker arg.pdb ../roux_ses.siz arg_roux_stern_5.srf 1.4 2.0 5 1 1 1 .
+cd ..
+
+cd asp
+~/repos/fftsvd/meshmaker asp.pdb ../scaled_charmm22.siz asp_scaledcharmm_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker asp.pdb ../scaled_charmm22.siz asp_scaledcharmm_stern_5.srf 1.4 2.0 5 1 1 1 .
+~/repos/fftsvd/meshmaker asp.pdb ../radii_fixedNH3_parse.siz asp_parse_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker asp.pdb ../radii_fixedNH3_parse.siz asp_parse_stern_5.srf 1.4 2.0 5 1 1 1 .
+~/repos/fftsvd/meshmaker asp.pdb ../roux_ses.siz asp_roux_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker asp.pdb ../roux_ses.siz asp_roux_stern_5.srf 1.4 2.0 5 1 1 1 .
+cd ..
+
+cd cys
+~/repos/fftsvd/meshmaker cys.pdb ../scaled_charmm22.siz cys_scaledcharmm_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker cys.pdb ../scaled_charmm22.siz cys_scaledcharmm_stern_5.srf 1.4 2.0 5 1 1 1 .
+~/repos/fftsvd/meshmaker cys.pdb ../radii_fixedNH3_parse.siz cys_parse_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker cys.pdb ../radii_fixedNH3_parse.siz cys_parse_stern_5.srf 1.4 2.0 5 1 1 1 .
+~/repos/fftsvd/meshmaker cys.pdb ../roux_ses.siz cys_roux_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker cys.pdb ../roux_ses.siz cys_roux_stern_5.srf 1.4 2.0 5 1 1 1 .
+cd ..
+
+cd glu
+~/repos/fftsvd/meshmaker glu.pdb ../scaled_charmm22.siz glu_scaledcharmm_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker glu.pdb ../scaled_charmm22.siz glu_scaledcharmm_stern_5.srf 1.4 2.0 5 1 1 1 .
+~/repos/fftsvd/meshmaker glu.pdb ../radii_fixedNH3_parse.siz glu_parse_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker glu.pdb ../radii_fixedNH3_parse.siz glu_parse_stern_5.srf 1.4 2.0 5 1 1 1 .
+~/repos/fftsvd/meshmaker glu.pdb ../roux_ses.siz glu_roux_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker glu.pdb ../roux_ses.siz glu_roux_stern_5.srf 1.4 2.0 5 1 1 1 .
+cd ..
+
+cd his
+~/repos/fftsvd/meshmaker his.pdb ../scaled_charmm22.siz his_scaledcharmm_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker his.pdb ../scaled_charmm22.siz his_scaledcharmm_stern_5.srf 1.4 2.0 5 1 1 1 .
+~/repos/fftsvd/meshmaker his.pdb ../radii_fixedNH3_parse.siz his_parse_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker his.pdb ../radii_fixedNH3_parse.siz his_parse_stern_5.srf 1.4 2.0 5 1 1 1 .
+~/repos/fftsvd/meshmaker his.pdb ../roux_ses.siz his_roux_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker his.pdb ../roux_ses.siz his_roux_stern_5.srf 1.4 2.0 5 1 1 1 .
+cd ..
+
+cd lys
+~/repos/fftsvd/meshmaker lys.pdb ../scaled_charmm22.siz lys_scaledcharmm_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker lys.pdb ../scaled_charmm22.siz lys_scaledcharmm_stern_5.srf 1.4 2.0 5 1 1 1 .
+~/repos/fftsvd/meshmaker lys.pdb ../radii_fixedNH3_parse.siz lys_parse_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker lys.pdb ../radii_fixedNH3_parse.siz lys_parse_stern_5.srf 1.4 2.0 5 1 1 1 .
+~/repos/fftsvd/meshmaker lys.pdb ../roux_ses.siz lys_roux_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker lys.pdb ../roux_ses.siz lys_roux_stern_5.srf 1.4 2.0 5 1 1 1 .
+cd ..
+
+cd tyr
+~/repos/fftsvd/meshmaker tyr.pdb ../scaled_charmm22.siz tyr_scaledcharmm_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker tyr.pdb ../scaled_charmm22.siz tyr_scaledcharmm_stern_5.srf 1.4 2.0 5 1 1 1 .
+~/repos/fftsvd/meshmaker tyr.pdb ../radii_fixedNH3_parse.siz tyr_parse_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker tyr.pdb ../radii_fixedNH3_parse.siz tyr_parse_stern_5.srf 1.4 2.0 5 1 1 1 .
+~/repos/fftsvd/meshmaker tyr.pdb ../roux_ses.siz tyr_roux_5.srf 1.4 2.0 5 1 1 0 .
+~/repos/fftsvd/meshmaker tyr.pdb ../roux_ses.siz tyr_roux_stern_5.srf 1.4 2.0 5 1 1 1 .
+cd ..
+
