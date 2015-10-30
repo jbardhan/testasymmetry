39272f2d3733cd331c9e8d61ce5fdf8ede40bb5a
Spencer <goossens.s@husky.neu.edu>
2015-10-30 11:57:39 -0400

    Ammended "Level_2.m" and "ObjectiveFunction.m" for the 20 ion cases (10 different ions, each with two cases -- plus and minus).

--- functions/ObjectiveFunction.m
+++ functions/ObjectiveFunction.m
@@ -3,7 +3,7 @@ function Error = ObjectiveFunction(i,Params,E)
 %               calculated FEP free energy.
 
 
-MD = [1 2 3 4 5 6 7 8 ];
+MD = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20];
 
 for i = 1 : length(MD)
     Error(i) = [(MD(i) - E(i)).^2];
