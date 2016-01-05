c52f0e5c3f0b193b5dbe994749afd833878d84ed
Spencer <goossens.s@husky.neu.edu>
2016-01-05 07:26:59 -0500

    Removed unnecessary functions and files "CalcEnergy.m, Differenceof.m, bornPicardNoStern2.m" as they were futile attempts at the first stage of the optimizer.  The file "Parameters_of_temp.m" is not useful now but may be useful later.  ************ The working automated optimizer consists of one file "Optimize_Params_at_temp.m".  All one has to do is input the temperatures that you would like to optimize at for the variable "t".  The code will then run through and optimize at each temperature and then save bot the parameters alpha, beta, gamma AND the temperature to a file called Parameters.mat.  Then, DeltaGvsT loads this file Parameters.mat and uses the variables saved within in the respective places.  All in all, DeltaGvsT does the same as before but now loads alpha, beta, gamma, and the temperatures automatically.

--- functions/DeltaGvsT.m
+++ functions/DeltaGvsT.m
@@ -2,6 +2,8 @@ addpath('..')
 addpath('../functions/')
 addpath('../born/')
 addpath('../../pointbem')
+load Parameters
+
 n = 10 ; % Number of picard iterations
 q1 = 1; % Positive charge
 q2 = -1; % Negative charge
@@ -9,76 +11,92 @@ k = 1; % kappa
 epsIn = 1;
 rscale = 0.92;
 conv_factor = 332.112;
-T = [0 5 10 15 20 25 30 35 40]+273.15; % Temperature in Kelvin
-A = [1.061 1.068 1.075 1.083 1.09 1.098 1.106 1.114 1.123]; % Values for alpha at each temp
-B = -1.*[118.542 120.061 121.606 123.157 124.734 126.394 128.008 129.657 131.343]; % Values for beta at each temp
-G = -1.*[.857 .872 .888 .904 .92 .937 .953 .969 .986]; % Values for gamma at each temp
+T = t+273.15; %[0 5 10 15 20 25 30 35 40]+273.15; % Temperature in Kelvin
+A = x(:,1)';   %[1.061 1.068 1.075 1.083 1.09 1.098 1.106 1.114 1.123]; % Values for alpha at each temp
+B = x(:,2)';   %-1.*[118.542 120.061 121.606 123.157 124.734 126.394 128.008 129.657 131.343]; % Values for beta at each temp
+G = x(:,3)';   %-1.*[.857 .872 .888 .904 .92 .937 .953 .969 .986]; % Values for gamma at each temp
 Ion = [1 2 3 4 5 6 7 8 9]; % Ion index for plotting purposes
 %   [Li Na K Rb Cs F Cl Br I]
-for i = 1:9
+
+%% Preallocation of variables
+epsOut(length(T)) = zeros;
+Params(length(T)) = struct('alpha',zeros, 'beta', zeros, 'EfieldOffset', zeros);
+L_eng(length(T)) = zeros;
+NA_eng(length(T)) = zeros;
+K_eng(length(T)) = zeros;
+RB_eng(length(T)) = zeros;
+CS_eng(length(T)) = zeros;
+F_eng(length(T)) = zeros;
+CL_eng(length(T)) = zeros;
+BR_eng(length(T)) = zeros;
+I_eng(length(T)) = zeros;
+
+
+%% Variables
+for i = 1:length(T)
 epsOut(i) = epsilon_t(T(i)); % Epsilon out as a function of temp
 Params(i) = struct('alpha', A(i), 'beta', B(i), 'EfieldOffset', G(i)); % Structured array of alpha, beta, gamma as a function of temp
 end
 
 %% lithiumPlus i = 1
 lithiumRminOver2 = 1.025; % All radii from Joung08 
-for l = 1:9
+for l = 1:length(T)
     L_eng(l) = bornPicardNoStern(lithiumRminOver2*rscale,q1,epsIn,epsOut(l),k,Params(l),conv_factor,n);
 end
 L1 = polyfit(T,L_eng,1); % Slope and intercept for Li
 F1 = polyval(L1,T); % Fitted line for Li
 %% sodiumPlus i = 2
 sodiumRminOver2 = 1.369; 
-for l = 1:9
+for l = 1:length(T)
     NA_eng(l) = bornPicardNoStern(sodiumRminOver2*rscale,q1,epsIn,epsOut(l),k,Params(l),conv_factor,n);
 end
 NA1 = polyfit(T,NA_eng,1);
 F2 = polyval(NA1,T);
 %% potassiumPlus  i = 3
 potassiumRminOver2 = 1.705; 
-for l = 1:9
+for l = 1:length(T)
     K_eng(l) = bornPicardNoStern(potassiumRminOver2*rscale,q1,epsIn,epsOut(l),k,Params(l),conv_factor,n);
 end
 K1 = polyfit(T,K_eng,1);
 F3 = polyval(K1,T);
 %% rubidiumPlus  i = 4
 rubidiumRminOver2 = 1.813;
-for l = 1:9
+for l = 1:length(T)
     RB_eng(l) = bornPicardNoStern(rubidiumRminOver2*rscale,q1,epsIn,epsOut(l),k,Params(l),conv_factor,n);
 end
 RB1 = polyfit(T,RB_eng,1);
 F4 = polyval(RB1,T);
 %% cesiumPlus i = 5
 cesiumRminOver2 = 1.976;
-for l = 1:9
+for l = 1:length(T)
     CS_eng(l) = bornPicardNoStern(cesiumRminOver2*rscale,q1,epsIn,epsOut(l),k,Params(l),conv_factor,n);
 end
 CS1 = polyfit(T,CS_eng,1);
 F5 = polyval(CS1,T);
 %% fluorineMinus i = 6
 fluorineRminOver2 = 2.303; 
-for l = 1:9
+for l = 1:length(T)
     F_eng(l) = bornPicardNoStern(fluorineRminOver2*rscale,q2,epsIn,epsOut(l),k,Params(l),conv_factor,n);
 end
 FL1 = polyfit(T,F_eng,1);
 F6 = polyval(FL1,T);
 %% chlorideMinus  i = 7
 chlorideRminOver2 = 2.513;
-for l = 1:9
+for l = 1:length(T)
     CL_eng(l) = bornPicardNoStern(chlorideRminOver2*rscale,q2,epsIn,epsOut(l),k,Params(l),conv_factor,n);
 end
 CL1 = polyfit(T,CL_eng,1);
 F7 = polyval(CL1,T);
 %% bromineMinus i = 8
 bromineRminOver2 = 2.608; 
-for l = 1:9
+for l = 1:length(T)
     BR_eng(l) = bornPicardNoStern(bromineRminOver2*rscale,q2,epsIn,epsOut(l),k,Params(l),conv_factor,n);
 end
 BR1 = polyfit(T,BR_eng,1);
 F8 = polyval(BR1,T);
 %% iodineMinus i = 9
 iodineRminOver2 = 2.860; 
-for l = 1:9
+for l = 1:length(T)
     I_eng(l) = bornPicardNoStern(iodineRminOver2*rscale,q2,epsIn,epsOut(l),k,Params(l),conv_factor,n);
 end
 I1 = polyfit(T,I_eng,1);
--- functions/Optimize_Params_at_temp.m
+++ functions/Optimize_Params_at_temp.m
@@ -1,25 +1,22 @@
-function Parameters = Parameters_of_temp(x)
-
 addpath('..')
 addpath('../born/')
 addpath('../../pointbem/')
 
 x0 = [0.5 -60 -0.5];
-lb = [0 -500 -100];
-ub = [200 0 0];
-
+lb = [0 -Inf -Inf];
+ub = [Inf 0 0];
+t = 0:40;
 options = optimoptions('lsqnonlin');
 options = optimoptions(options,'Display', 'off');
 
-t = 5:6;
+x(length(t),3) = zeros;
 
 
 for i = 1:length(t)
-   
-Parameters = ObjectiveFunction_MSA(x,t(i));
-
+y = @(x)ObjectiveFunction_MSA(x,t(i));
 [x(i,:),resnorm,residual,exitflag,output,lambda,jacobian] = ...
-lsqnonlin(@Parameters_of_temp,x0,lb,ub,options);
+lsqnonlin(y,x0,lb,ub,options);
+display(['Optimizing at ' , num2str(t(i)), ' degrees Celcius'])
+end
 
-end
-end
+save('Parameters','x','t')
