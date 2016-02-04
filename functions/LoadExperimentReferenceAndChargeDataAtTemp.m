0d1da9f1d75cca079c10f7f64de6c5aedf685b26
Spencer <goossens.s@husky.neu.edu>
2016-02-03 19:42:20 -0500

    Added a script that mirrors you ParamSternIons.m but mine is a function of Temp.  It is called ParamSternIonsAgainstExptAsFunctionOfTemp.m  This script calls LoadExperimentReferenceAndChargeDataAtTemp.m which calculates Delta G as a function of Temperature.  Params_NLBC_20C are the parameters I got when running the scripts at 20C.

---
+++ functions/LoadExperimentReferenceAndChargeDataAtTemp.m
@@ -0,0 +1,19 @@
+delS = [-133 -96 -87 -81 -53].*(0.239/10^3)';
+delG = [-424 -352 -329 -306 -304].*0.239';
+t = 20; 
+delt = t-25; % Celcius
+
+
+delG_new = [delG(1)-delt*delS(1)
+            delG(2)-delt*delS(2)
+            delG(3)-delt*delS(3)
+            delG(4)-delt*delS(4)
+            delG(5)-delt*delS(5)];
+
+NaReference = delG_new(1);
+KReference = delG_new(2);
+RbReference = delG_new(3);
+CsReference = delG_new(4);
+ClReference = delG_new(5);
+
+chargeDistribution = [1 -1];
