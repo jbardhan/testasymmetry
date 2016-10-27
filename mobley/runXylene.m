142db487970da0f96912c773485eaf65cd7116b8
Jay Bardhan <j.bardhan@neu.edu>
2016-10-27 07:20:09 -0400

    added runXylene, updated paramCyclohexane (don't run paramForCyclohexane... not sure why keeping it) and runCyclohexane.

---
+++ mobley/runXylene.m
@@ -0,0 +1,54 @@
+% Path information
+addpath('/Users/jbardhan/repos/pointbem');
+addpath('/Users/jbardhan/repos/panelbem');
+addpath('/Users/jbardhan/repos/testasymmetry');
+addpath('/Users/jbardhan/repos/testasymmetry/functions');
+addpath('/Users/jbardhan/repos/testasymmetry/mobley');
+addpath('/Users/jbardhan/repos/testasymmetry/born/');
+
+% a bunch of useful variables and constants. also defining the global
+% variable "ProblemSet" which we'll use to hold the BEM systems.
+loadConstants
+convertKJtoKcal = 1/joulesPerCalorie;
+
+global UsefulConstants ProblemSet saveMemory writeLogfile logfileName
+logfileName = 'xylene.out';
+epsOut = 2.3879; % Zhao+Abraham J. Org. Chem 2005
+x = [0.6773 -71.2787 -2.0742 4.4332 -0.0209 1.8286];
+
+epsIn  =  1;
+Tbase = 300; mytemp=Tbase;
+
+saveMemory = 1;
+writeLogfile = 1;
+
+KelvinOffset = 273.15;
+conv_factor = 332.112;
+staticpotential = 0.0; % this only affects charged molecules;
+
+kappa = 0.0;  % should be zero, meaning non-ionic solutions!
+UsefulConstants = struct('epsIn',epsIn,'epsOut',epsOut,'kappa', ...
+			 kappa,'conv_factor',conv_factor,...
+			 'staticpotential',staticpotential);
+
+% here we define the actual params for the NLBC test
+asymParams = struct('alpha',0.5, 'beta', -100,'EfieldOffset',1); 
+     
+[mol_list,dG_list,surfArea_list]=textread('mnsol/xylene.csv',...
+					  '%s %f %f','delimiter',',');
+
+curdir = pwd;
+for i=20:length(mol_list)
+  dir=sprintf('%s/Dropbox/lab/projects/slic-jctc-mnsol/nlbc-mobley/nlbc_test/%s',getenv('HOME'),mol_list{i});
+  chdir(dir);
+  pqrData = loadPqr('test.pqr');
+  pqrAll{i} = pqrData;
+  srfFile{i} = sprintf('%s/test_2.srf',dir);
+  chargeDist{i} = pqrData.q;
+  referenceData{i} = dG_list(i);
+  surfArea{i} = surfArea_list(i);
+  chdir(curdir);
+  addProblemSA(mol_list{i},pqrAll{i},srfFile{i},chargeDist{i},referenceData{i},surfArea{i});
+end
+
+[errfinal,calcE,refE,es,np]=ObjectiveFromBEMSA(x);
