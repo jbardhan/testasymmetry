781ece4e0a403da6a0d26b271f8b903c3c042f30
Jaydeep Bardhan <jbardhan@alum.mit.edu>
2016-10-27 14:31:17 -0400

    Param scripts for carbontet, hexadecane, and toluene.  Fixed run scripts for benzene and toluene.  Need to be checked

---
+++ mobley/paramHexadecane.m
@@ -0,0 +1,87 @@
+% Path information
+addpath('/Users/jbardhan/repos/pointbem');
+addpath('/Users/jbardhan/repos/panelbem');
+addpath('/Users/jbardhan/repos/testasymmetry');
+addpath('/Users/jbardhan/repos/testasymmetry/functions');
+addpath('/Users/jbardhan/repos/testasymmetry/mobley');
+addpath('/Users/jbardhan/repos/testasymmetry/born/');
+
+% a bunch of useful variables and constants. also defining the global
+% variable "ProblemSet" which we'll use to hold the BEM systems.
+loadConstants
+convertKJtoKcal = 1/joulesPerCalorie;
+global UsefulConstants ProblemSet saveMemory writeLogfile logfileName
+saveMemory = 0;
+writeLogfile = 0;
+logfileName = 'junklogfile';
+
+epsIn  =  1;
+Tbase = 300; 
+epsOut = 2.0402; % from MNSol
+
+mytemp=Tbase;
+KelvinOffset = 273.15;
+conv_factor = 332.112;
+staticpotential = 0.0; % this only affects charged molecules;
+kappa = 0.0;  % should be zero, meaning non-ionic solutions!
+
+
+% the staticpotential below should not be used any more, please check
+UsefulConstants = struct('epsIn',epsIn,'epsOut',epsOut,'kappa', ...
+			 kappa,'conv_factor',conv_factor,...
+			 'staticpotential',staticpotential);
+     
+[mol_list,dG_list,surfArea_list]=textread('mnsol/hexadecane.csv',...
+					  '%s %f %f','delimiter',',');
+
+
+testset  = {'acetic_acid','ethanol','methanol','p_cresol','propanoic_acid',...
+	    'toluene','n_octane','ethylamine','14_dioxane'};
+
+% all octanol available side chain analogues 
+%testset = {'2_methylpropane', 'acetic_acid', 'ethanol', 'methane', 'methanol',...
+% 'n_butane', 'n_butylamine', 'p_cresol', 'propane', 'propanoic_acid','toluene'};
+
+% complete list of side chain analogues. not available for all solvents
+%testset = {'1_methyl_imidazole','2_methylpropane', ...
+%	   '3_methyl_1h_indole','acetic_acid','ethanamide', ...
+%	   'ethanol','methane','methanethiol','methanol', ...
+%	   'methyl_ethyl_sulfide','n_butane','n_butylamine', ...
+%	   'p_cresol','propane','propanoic_acid','toluene'};
+curdir=pwd;
+for i=1:length(testset)
+  dir=sprintf('%s/Dropbox/lab/projects/slic-jctc-mnsol/nlbc-mobley/nlbc_test/%s',getenv('HOME'),testset{i});
+  chdir(dir);
+  pqrData = loadPqr('test.pqr');
+  pqrAll{i} = pqrData;
+  srfFile{i} = sprintf('%s/test_2.srf',dir);
+  chargeDist{i} = pqrData.q;%chargeDistribution;
+  foo = strcmp(mol_list,testset{i});
+  index = find(foo);
+  if length(index) ~= 1
+    fprintf('error finding refdata!\n');
+    keyboard
+  end
+  referenceData{i} = dG_list(index);
+  surfArea{i} = surfArea_list(index);
+  chdir(curdir);
+  addProblemSA(testset{i},pqrAll{i},srfFile{i},chargeDist{i},referenceData{i},surfArea{i});
+end
+
+pqrData = struct('xyz', [0 0 0], 'q', 1, 'R', 1);
+
+% The following script is specialized to this example.  We'll
+% handle generating others.  Not complicated, but it's not self-explanatory.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+x0 = [0.5  -60  -0.5 0.0 -0.03 1.6];
+lb = [-2 -200 -100 -20   -0.1  0];
+ub = [+2 +200 +100 +20   +0.1 +4];
+
+options = optimoptions('lsqnonlin','MaxIter',4);
+options = optimoptions(options,'Display', 'iter');
+
+y = @(x)ObjectiveFromBEMSA(x);
+[x,resnorm,residual,exitflag,output,] = lsqnonlin(y,x0,lb,ub,options);
