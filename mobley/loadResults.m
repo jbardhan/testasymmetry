893e134bb45aaa1d04354fe71cfe16d82720205d
Spencer Goossens <sgoossens18@gmail.com>
2017-04-12 13:55:01 -0400

    Fixed the axes for the plotting routine

--- mobley/loadResults.m
+++ mobley/loadResults.m
@@ -2,17 +2,20 @@ close all; clear all
 ploton = 1;
 solvents = {'Water', 'Octanol', 'Hexadecane', 'Chloroform', 'Cyclohexane',...
             'Carbontet', 'Hexane', 'Toluene', 'Xylene'}; 
+        
 testset  = {'acetic_acid', 'ethanol', 'methanol', 'p_cresol',...
-    'propanoic_acid', 'toluene', 'ethylamine', 'n_octane', 'pyridine',...
-    'nitromethane', 'heptan_1_ol', 'n_butyl_acetate'};
-
+        'propanoic_acid', 'toluene', 'ethylamine', 'n_octane', 'pyridine',...
+        'nitromethane', 'heptan_1_ol', 'n_butyl_acetate'};
 addpath('export_fig/')
+
          
 for i = 1:length(solvents)
+           
     fid = fopen(['mnsol/',lower(solvents{i}),'.csv'],'r'); 
     Data = textscan(fid,'%s %f %f','delimiter',',');
     fclose(fid);
     mol_list = Data{1};
+
     [~,m] = ismember(testset,mol_list);
     temp = load(['Run',solvents{i}]);
     sorted_errors = sort(abs(temp.errfinal));
@@ -21,15 +24,15 @@ for i = 1:length(solvents)
                         temp.np,'refE',temp.refE,'RMS',rms(temp.calcE-temp.refE));
     if ploton
         figure
-        plot(temp.calcE,temp.refE,'bx','markers',12)
+        plot(temp.refE,temp.calcE,'bx','markers',12)
         set(gca,'FontSize',15)
         hold on
-        plot(temp.calcE(m),temp.refE(m),'rx','markers',12)
-        plot([min(temp.calcE) max(temp.calcE)] , [min(temp.refE) max(temp.refE)],...
+        plot(temp.refE(m),temp.calcE(m),'rx','markers',12)
+        plot([min(temp.refE) max(temp.refE)] , [min(temp.calcE) max(temp.calcE)],...
                 'k-')
-        xlabel('Calculated Solvation Free Energies')
-        ylabel('Experimental Solvation Free Energies')
-        title(['Experiment vs. Calculated Solvation Free Energies for ',...
+        xlabel('Experimental Solvation Free Energies')
+        ylabel('Calculated Solvation Free Energies')
+        title(['Calculated vs. Experiment Solvation Free Energies for ',...
                 solvents{i}])
       filename = sprintf('Output/DeltaG-%s.PDF',solvents{i});
       export_fig(filename,'-painters','-transparent');
