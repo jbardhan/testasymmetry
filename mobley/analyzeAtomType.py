217e3006f46dbd2bcbb93b663d42c1a57ed31b8a
Spencer Goossens <sgoossens18@gmail.com>
2017-04-14 13:09:04 -0400

    Created a python script that produces a .csv file called errorByAtomTypeFor<Solvent>.csv.  This csv file contains three columns.  The first is the absolute value of the error between SLIC calculated solvation free energies and reference solvation free energies.  The next column is the solute name followed by the atom types in this solute.  Thus if one orders the errorByAtomTypeFor<Solvent>.csv file by descending error, it is easy to see the atom types that contribute to the hughest errors.
    To run analyzeAtomType.py, simply select the solvents you would like to analyze and ut them in the array solventList.  Then run!

--- mobley/analyzeAtomType.py
+++ mobley/analyzeAtomType.py
@@ -1,15 +1,51 @@
-dataFile = open('Water-Data.txt','r')
-soluteNames = open('mnsol/water.csv','r')
+import os
+import pandas as pd
+Home = os.path.expanduser('~')
+prmPath = Home + '/Dropbox/lab/Projects/slic-jctc-mnsol/nlbc-mobley/prmcrd'
+csvPath = Home + '/repos/testasymmetry/mobley/'
 
-errors = []
-for line in dataFile:
-    if line.split(',')[1].strip() != 'errfinal':
-        errors.append(line.split(',')[1].strip())
+solventList = ['Water','Octanol','Chloroform','Hexane','Xylene','Hexadecane','Cyclohexane','Toluene','Carbontet']
+for solvent in solventList:
+    dataFile = open((solvent+'-Data.txt'),'r')
+    soluteNames = open(('mnsol/'+solvent.lower()+'.csv'),'r')
 
-solutes = []   
-for line in soluteNames:
-    solutes.append(line.split(',')[0])
+    errors = [] # Get the errors from the Solvent-Data.txt file
+    for line in dataFile:
+        if line.split(',')[1].strip() != 'errfinal':
+            errors.append(line.split(',')[1].strip())
 
-dataFile.close()
-soluteNames.close()
+    solutes = [] # Get the solute names from the solvent.csv file
+    for line in soluteNames:
+        solutes.append(line.split(',')[0])
+    dataFile.close()
+    soluteNames.close()
+    
+    typeLineIndex = [] # Get the line index for atom types from each solute.prtmtop file for all solutes from solvent.csv
+    for solute in solutes:
+        prmFile = open(os.path.join(prmPath,(solute+'.prmtop')))
+        for i, line in enumerate(prmFile):
+            if 'FLAG AMBER_ATOM_TYPE' in line:
+                typeLineIndex.append(i + 2)            
+        prmFile.close()
 
+    index = 0  
+    atomTypes = [] # gets the atom types in each prmtop file
+    for solute in solutes:
+        prmFile = open(os.path.join(prmPath,(solute+'.prmtop')))
+        lines = prmFile.readlines()
+        temp = lines[typeLineIndex[index]].strip()
+        atomTypes.append(temp.split())
+        prmFile.close()
+        index += 1
+        
+    errDict = {} # Creates a dictionary with keys = abs(error) and vals = [solute_name, atom_types]
+    index = 0
+    for error in errors:
+        data = [solutes[index],atomTypes[index]]
+        errDict[abs(float(error))] = data
+        index += 1
+        
+    colNames = ['Solute Name', 'Atom Type']
+    dFrame = pd.DataFrame.from_dict(errDict,orient='index')
+    dFrame.columns = colNames
+    dFrame.to_csv(os.path.join(csvPath,('errorByAtomTypeFor'+solvent+'.csv')),header=True) # Stores the dictionary as a csv file    
--- mobley/convertMatToText.m
+++ mobley/convertMatToText.m
@@ -1,10 +1,10 @@
 solvents = {'Water', 'Octanol', 'Hexadecane', 'Chloroform', 'Cyclohexane',...
             'Carbontet', 'Hexane', 'Toluene', 'Xylene'}; 
-        
+solvents = {'Water'};
 for i = 1:length(solvents)
    data = load(['Run',solvents{i}]);
    fid = fopen([solvents{i},'-Data.txt'],'w');
    fprintf(fid,'calcE, errfinal, es, np, refE\n');
-   fprintf(fid,'%f, %f, %f, %f, %f \n',[calcE,errfinal,es,np,refE].');
+   fprintf(fid,'%f, %f, %f, %f, %f \n',[data.calcE,data.errfinal,data.es,data.np,data.refE].');
    fclose(fid);
 end
