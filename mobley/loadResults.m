d40284e727cfac287f26a19c895e96f63508d9bb
Spencer Goossens <sgoossens18@gmail.com>
2017-04-20 13:09:30 -0400

    looping in the changes made in Mobley_Surface_Area to this branch.  These changes are the ability to writie out tables for errors and create a histogram of errors.

--- mobley/loadResults.m
+++ mobley/loadResults.m
@@ -1,9 +1,26 @@
 close all; clear all
+addpath('export_fig/')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%% Set your toggles and define the solute and solvent lists %%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ploton = 1;
+
 solvents = {'Water', 'Octanol', 'Hexadecane', 'Chloroform', 'Cyclohexane',...
             'Carbontet', 'Hexane', 'Toluene', 'Xylene'}; 
-addpath('export_fig/')
-         
+        
+common_solutes = {'ethanol','butanone','toluene','n_octane','nitromethane',...
+            '14_dioxane','phenol','acetic_acid','methanol','propanoic_acid',...
+            'propan_1_ol','ethyl_acetate','aniline','ethylamine','n_butyl_acetate',...
+            'methyl_acetate','hexan_1_ol','pentan_1_ol','n_propyl_acetate','butan_1_ol',...
+            'heptan_1_ol','methyl_pentanoate','p_cresol','methyl_propanoate',...
+            'o_cresol','propanone','pyridine'};
+        
+% This routine will plot all of the calulated vs. experimental results if 
+% plot is set to 1.  Futhermore, it creates structures that contain all of
+% the results for calculated solvation free energies as a function of each 
+% solvent and again as a function of solute        
 for i = 1:length(solvents)
     if string(solvents{i}) ~= 'Water'
             testset  = {'acetic_acid', 'ethanol', 'methanol', 'p_cresol',...
@@ -24,11 +41,19 @@ for i = 1:length(solvents)
     end
     
     [~,m] = ismember(testset,mol_list);
+    [~,n] = ismember(common_solutes,mol_list);
+    
     temp = load(['Run',solvents{i}]);
+    solute_errors(i,:) = temp.errfinal(n);
     sorted_errors = sort(abs(temp.errfinal));
-    results(i) = struct('Solvent', solvents{i},'CalcE',temp.calcE,...
+    results(i) = struct('Solvent', solvents{i},'Num_Solutes',round(length(mol_list),3),'CalcE',temp.calcE,...
                         'errfinal',temp.errfinal,'es',temp.es,'np',...
-                        temp.np,'refE',temp.refE,'RMS',rms(temp.calcE-temp.refE));
+                        temp.np,'refE',temp.refE,'RMS',rms(temp.calcE-temp.refE),...
+                        'Mean_Abs_error',mean(abs(temp.errfinal)),'RMS_Training',...
+                        rms(temp.errfinal(m)),'Mean_Abs_error_Training',...
+                        mean(abs(temp.errfinal(m))),'RMS_Cons',...
+                        rms(temp.errfinal(n)),'Mean_Abs_error_Cons',...
+                        mean(abs(temp.errfinal(n))));
     if ploton
         figure
         plot(temp.refE,temp.calcE,'bx','markers',12)
@@ -41,6 +66,7 @@ for i = 1:length(solvents)
         ylabel('Calculated Solvation Free Energies')
         title(['Calculated vs. Experiment Solvation Free Energies for ',...
                 solvents{i}])
+        legend('Calculated','Training Set','Experiment','Location','southeast')
       filename = sprintf('Output/DeltaG-%s.PDF',solvents{i});
       export_fig(filename,'-painters','-transparent');
 
@@ -51,6 +77,33 @@ for i = 1:length(solvents)
         m1, 'Second_Max',m2);
     clear temp 
 end
+
+
+solute_errors = transpose(solute_errors);
+
+% Plot a histogram of errors
+if ploton
+    figure 
+    nbins = 25;
+    for i = 1:length(results)
+        subplot(3,3,i);
+        histogram(results(i).errfinal,nbins)
+        title(['Errors for ',results(i).Solvent])
+        xlabel('Error')
+        ylabel('Number of Occurances') 
+    end
+    filename = sprintf('Output/HistogramOfErrors.PDF');
+    export_fig(filename,'-painters','-transparent');
+end
+
+% Create a structure conataining the errors associated with each solute
+for i = 1:length(common_solutes)
+    solute_struct(i) = struct('Solute_Name',common_solutes(i),'RMS',rms(solute_errors(i,:)),...
+        'Mean_Abs_error',mean(abs(solute_errors(i,:))));
+end
+
+writeDat('SolventErrors.tex',results);
+writeDat('SoluteErrors.tex',solute_struct);
 
 Outliers = readErr(max_err);
 save('Solvent Outliers','Outliers');
