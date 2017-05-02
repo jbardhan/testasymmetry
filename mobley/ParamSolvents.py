103311d23766ad50a0b50f7f375b3ea6142a5392
Ali <mehdizadehrahimi.a@husky.neu.edu>
2017-05-02 10:42:05 -0400

    test commit

--- mobley/ParamSolvents.py
+++ mobley/ParamSolvents.py
@@ -4,18 +4,18 @@ import os
 Home = os.environ['HOME']
 os.chdir(Home+'/repos/testasymmetry/mobley')
 
-Run_Type = 'run'
+Run_Type = 'param'
 
 if Run_Type == 'param':
-	Solvent_List = ['paramWater', 'paramOctanol', 'paramHexadecane', 'paramChloroform',
- 		'paramCyclohexane','paramCarbontet', 'paramHexane', 'paramToluene', 'paramXylene']
+	Solvent_List = ['paramWater', 'paramOctanol', 'paramMethanol', 'paramAcetonitrile', 'paramNitromethane', 'paramPropanol',
+ 		'paramDichloroethane','paramDimethylsulfoxide', 'paramDimethylformamide', 'paramEthanol', 'paramNitrobenzene']
 elif Run_Type == 'run':
-	Solvent_List = ['runWater', 'runOctanol', 'runHexadecane', 'runChloroform',
- 		'runCyclohexane','runCarbontet', 'runHexane', 'runToluene', 'runXylene']
+	Solvent_List = ['runWater', 'runOctanol', 'runMethanol', 'runAcetonitrile', 'runNitromethane', 'runPropanol',
+ 		'runDichloroethane','runDimethylsulfoxide', 'runDimethylformamide', 'runEthanol', 'runNitrobenzene']
 
-Solvent_List = Solvent_List[3:]
+Solvent_List = Solvent_List[:]
 
-if len(Solvent_List) <= 9:
+if len(Solvent_List) <= 11:
 	for solvent in Solvent_List:
 		eng = matlab.engine.start_matlab()
 		eng.eval(solvent,nargout=0)
