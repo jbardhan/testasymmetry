ad3a8e6632568d815451f5175418f67c456ec0ed
Ali Mehdizadeh Rahimi <mehdizadehrahimi.a@husky.neu.edu>
2017-05-08 18:20:05 -0400

    Added Butanol results and modified paramNitromethane

--- mobley/paramNitromethane.m
+++ mobley/paramNitromethane.m
@@ -49,7 +49,7 @@ all_surfAreas = Data{2};
 surfArea_list = all_surfAreas(index);
 
 %testset  = {'acetic_acid', 'ethanol', 'methanol', 'p_cresol', 'propanoic_acid', 'toluene', 'ethylamine', 'n_octane', 'pyridine', 'nitromethane', 'heptan_1_ol', 'n_butyl_acetate'};
-testset  = {'butanone','14_dioxane','n_octane','ethanol','nitromethane','toluene','Li','Na','K','Rb','Cs','Cl','Br','I'};
+testset  = {'butanone','14_dioxane','n_octane','ethanol','nitromethane','toluene','Li','Na','K','Cl','Br','I'};
 
 % all octanol available side chain analogues 
 %testset = {'2_methylpropane', 'acetic_acid', 'ethanol', 'methane', 'methanol',...
@@ -88,9 +88,9 @@ end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-x0 = [0.5 -60 -0.5   -0.5*tanh(- -0.5)     0 -0.03 1.6];
-lb = [-2 -200 -100 -20  -20  -0.1  -2];
-ub = [+2 +200 +100 +20  +20  +0.1  +2];
+x0 = [0.5 -60 -0.5   -0.5*tanh(- -0.5)     0 -0.03 0];
+lb = [-2 -200 -100 -20  -20  -0.1  -4];
+ub = [+2 +200 +100 +20  +20  +0.1  +4];
 
 options = optimoptions('lsqnonlin','MaxIter',8);
 options = optimoptions(options,'Display', 'iter');
