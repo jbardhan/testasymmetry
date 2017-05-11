9019153db7aedc60d62f83c163be4f6aad3c693b
Ali <mehdizadehrahimi.a@husky.neu.edu>
2017-05-11 14:06:06 -0400

    adding results for testset1

--- mobley/ParamSolvents.py
+++ mobley/ParamSolvents.py
@@ -7,15 +7,13 @@ os.chdir(Home+'/repos/testasymmetry/mobley')
 Run_Type = 'run'
 
 if Run_Type == 'param':
-	Solvent_List = ['paramButanol', 'paramWater', 'paramOctanol', 'paramMethanol', 'paramAcetonitrile', 'paramPropanone', 'paramPropanol',
- 		        'paramDichloroethane','paramDimethylsulfoxide', 'paramDimethylformamide', 'paramEthanol']
+	Solvent_List = ['paramButanol', 'paramWater', 'paramOctanol', 'paramMethanol', 'paramPropanol', 'paramEthanol']
 elif Run_Type == 'run':
-	Solvent_List = ['runButanol', 'runWater', 'runOctanol', 'runMethanol', 'runAcetonitrile', 'runPropanone', 'runPropanol',
- 		        'runDichloroethane','runDimethylsulfoxide', 'runDimethylformamide', 'runEthanol']
+	Solvent_List = ['runButanol', 'runWater', 'runOctanol', 'runMethanol', 'runPropanol', 'runEthanol']
 
-Solvent_List = Solvent_List[1:]
+Solvent_List = Solvent_List[:]
 
-if len(Solvent_List) <= 11:
+if len(Solvent_List) <= 6:
 	for solvent in Solvent_List:
 		eng = matlab.engine.start_matlab()
 		eng.eval(solvent,nargout=0)
