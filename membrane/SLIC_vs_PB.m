1e9def34f8485078ebb49845414c94cd72443a76
Ali <mehdizadehrahimi.a@husky.neu.edu>
2018-10-22 16:30:43 -0400

    Added a function that plots the SLIC-predicted vs. classical electrostatic theory's free energy changes of a positive ion moving vertically from the core of a bilayer membrane towards its top surface. The bilayer membrane is mimicked by a cylinder of radius 10 A.

---
+++ membrane/SLIC_vs_PB.m
@@ -0,0 +1,27 @@
+
+curdir = pwd;
+i=10;
+fileName = sprintf("%s/%d/%s%d%s",curdir,i,'RunMembrane_',i,'.mat');
+S = load(fileName);
+fileNamePB = sprintf("%s/%d/%s%d%s",curdir,i,'RunMembranePB_',i,'.mat');
+SPB = load(fileNamePB);
+B = {S.es.',SPB.es.'};   
+x=(25:-2.5:0);
+colRed = colormap(hot(8));
+colGreen = colormap(summer);
+
+hL =zeros(2,1);
+face = {colGreen(6,:),colRed(4,:)};
+style={'-o','-s'};
+for i=1:2
+hL(i) = plot(x,B{i},style{i},...
+        'Color',face{i},...
+        'LineWidth',2,...
+        'MarkerEdgeColor','k',...
+        'MarkerFaceColor',face{i},...
+        'MarkerSize',10);
+hold on
+end
+xlabel('Membrane depth (A)')
+ylabel('Electrostatic energy (kcal/mol)') 
+hLeg = legend(hL,'SLIC','PB');
