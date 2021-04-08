a43a91961b6a51685201dc91a496abb08dc39865
alineu <alimehdirahim@gmail.com>
2021-04-08 00:27:32 -0400

    paper runs

--- mobley/slic-COSMO/paramCosmo_Bondii.m
+++ mobley/slic-COSMO/paramCosmo_Bondii.m
@@ -37,7 +37,7 @@ allHbondTypes = {'n_amine','n_amide','n_nitro',...
              
 epsOutWater = 78.34;% from mnsol Database
 epsIn  =  1;
-epsOut = 12.85;%Wakai05 and Krossing06  ;Data_eps.Var2(14);
+epsOut = epsOutWater;%Wakai05 and Krossing06  ;Data_eps.Var2(14);
 KelvinOffset = 273.15;
 conv_factor = 332.112;
 staticpotential = 0.0; % this only affects charged molecules;
@@ -72,18 +72,19 @@ dG_disp_mob = allData.disp_mobley;
 dG_cav_mob = allData.cav_mobley; 
 dG_np_mob = allData.np_mobley; 
 dG_es_mob = allData.es_mobley; 
+dG_es_best = allData.best_elec; 
 dG_np_SLIC = allData.np_SLIC; 
 dG_es_SLIC = allData.es_SLIC; 
 all_solutes = allData.solute;
 mol_list = all_solutes;
 solventAreas = allData{495,9:79};
 solventATypes = allData{495,80:144};
-solventHbData = allData{495,145:end};
+solventHbData = allData{495,145:158};
 solventVdWA = allData{495,11};
 solventVdWV = allData{495,12};
 soluteAreas = allData{:,9:79};
 soluteATypes = allData{:,80:144};
-soluteHbData = allData{:,145:end};
+soluteHbData = allData{:,145:158};
 soluteVdWA = allData{:,11};
 soluteVdWV = allData{:,12};
 temperature = 24.85 + KelvinOffset;
@@ -104,8 +105,8 @@ for i=1:length(training_set)
   end
   soluteAtomAreas{i} = allData{index,9:79};
   soluteAtomTypes{i} = {allData{index,80:144}};
-  soluteHbondData{i} = allData{index,145:end};
-  referenceData{i} = dG_list(index);
+  soluteHbondData{i} = allData{index,145:158};
+  referenceData{i} = dG_list(index) - dG_es_best(index);
   solute_VdWA{i} = allData{index,11};
   solute_VdWV{i} = allData{index,12};
   solventAtomAreas{i} = solventAreas;
@@ -114,22 +115,97 @@ for i=1:length(training_set)
   solvent_VdWA{i} = solventVdWA;
   solvent_VdWV{i} = solventVdWV;
   atom_vols{i} = allData{index,14};
+  newHB{i}=0; 
+     
   temp{i} = temperature;
-  newHB{i} = 0;
   chdir(curdir);
-  addProblemCosmo(training_set{i},pqrAll{i},srfFile{i},chargeDist{i},referenceData{i},...
-                  soluteAtomAreas{i},soluteAtomTypes{i},soluteHbondData{i},...
-                  solute_VdWV{i},solute_VdWA{i},...
-                  solventAtomAreas{i},solventAtomTypes{i},solventHbondData{i},...
-                  solvent_VdWV{i},solvent_VdWA{i},...
-                  atom_vols{i},temp{i},newHB{i});
+  addProblemCosmoNpHB(training_set{i},chargeDist{i},referenceData{i},...
+      soluteAtomAreas{i},soluteAtomTypes{i},soluteHbondData{i},...
+      solute_VdWV{i},solute_VdWA{i},...
+      solventAtomAreas{i},solventAtomTypes{i},solventHbondData{i},...
+      solvent_VdWV{i},solvent_VdWA{i},...
+      atom_vols{i},temp{i},newHB{i});
 end
 
 
-% The following script is specialized to this example.  We'll
-% handle generating others.  Not complicated, but it's not self-explanatory.
+% The following script is specialized to this example. We'll
+% handle generating others. Not complicated, but it's not self-explanatory.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[~,id]=ismember(training_set,mol_list);
+disp_mob = allData.disp_mobley(id); 
+cav_mob = allData.cav_mobley(id); 
+np_mob = allData.np_mobley(id); 
+es_mob = allData.es_mobley(id); 
+np_SLIC = allData.np_SLIC(id); 
+es_SLIC= allData.es_SLIC(id);
+
+
+x0_1 = [0.453 -48.813 -0.541 -0.548 -0.062 0.81 1.02 1.05 1.74 0.55 0.13 0.00 0.49 0.78 ... % disp x(6:14)  
+0.37 1.54 0.06 0.38 0.72 0.55 0.35 1.10 0.34 ... % disp x(15:23)  
+0.59 0.09 0.22 0.08 0.05 0.07 0.35 0.83 0.11 1.19 3.74 0.04 ... % hb x(24:35)
+1.44 ... % comb x(36)      
+2.69 ]; % cav x(37)      
+
+x0_2 =[0.453 -48.813 -0.541 -0.548 -0.062 0.23 0.47 0.57 0.38 0.22 0.74 0.74 0.61 0.30 ... % disp x(6:14)    
+   0.36 0.63 0.40 0.60 0.97 0.27 0.04 0.78 0.83 ... % disp x(15:23)    
+   0.60 0.43 0.74 0.28 0.30 0.87 0.15 0.59 0.52 0.33 0.26 0.35 ... % hb x(24:35) 
+   5.96 ... % comb x(36)            
+   0.65 ]; % cav x(37)            
+x0_3 =[0.453 -48.813 -0.541 -0.548 -0.062 0.57 0.83 0.78 0.29 0.45 0.88 0.64 0.07 0.18 ... % disp x(6:14)    
+   0.34 0.24 0.58 0.04 0.25 0.19 0.95 0.95 0.44 ... % disp x(15:23)    
+   0.79 0.22 0.55 0.48 0.39 0.75 0.69 0.17 0.67 0.50 0.54 0.34 ... % hb x(24:35) 
+   1.67 ... % comb x(36)            
+   0.19 ]; % cav x(37)            
+x0_4 =[0.453 -48.813 -0.541 -0.548 -0.062 0.27 0.53 0.96 0.21 0.10 0.11 0.45 0.84 0.01 ... % disp x(6:14)    
+   0.33 0.65 0.42 0.97 0.96 0.64 0.29 0.69 0.84 ... % disp x(15:23)    
+   0.81 0.34 0.09 0.47 0.66 0.67 0.36 0.03 0.82 0.32 0.85 0.24 ... % hb x(24:35) 
+   6.64 ... % comb x(36)            
+   0.25 ]; % cav x(37)            
+x0_5 =[0.453 -48.813 -0.541 -0.548 -0.062 0.31 0.84 0.94 0.28 0.68 0.18 0.27 0.92 0.15 ... % disp x(6:14)    
+   0.35 0.74 0.59 0.53 0.88 0.50 0.74 0.64 0.11 ... % disp x(15:23)    
+   0.93 0.93 0.79 0.39 0.15 0.98 0.34 0.64 0.87 0.35 0.43 0.79 ... % hb x(24:35) 
+   0.54 ... % comb x(36)            
+   0.42 ]; % cav x(37)            
+x0_6 =[0.453 -48.813 -0.541 -0.548 -0.062 0.09 0.57 0.01 0.33 0.08 0.39 0.63 0.29 0.54 ... % disp x(6:14)    
+   0.11 0.66 0.00 0.92 0.22 0.78 0.90 0.82 0.82 ... % disp x(15:23)    
+   0.64 0.43 0.36 0.06 0.24 0.55 0.74 0.39 0.24 0.32 0.94 0.76 ... % hb x(24:35) 
+   2.34 ... % comb x(36)            
+   0.44 ]; % cav x(37)            
+x0_7 =[0.453 -48.813 -0.541 -0.548 -0.062 0.83 0.80 0.48 0.03 0.00 0.59 0.70 0.03 0.77 ... % disp x(6:14)    
+   0.93 0.19 0.84 0.71 0.49 0.99 0.81 0.69 0.46 ... % disp x(15:23)    
+   0.28 0.80 0.66 0.76 0.89 0.52 0.36 0.23 0.23 0.14 0.41 0.25 ... % hb x(24:35) 
+   9.96 ... % comb x(36)            
+   0.37 ]; % cav x(37)            
+x0_8 =[0.453 -48.813 -0.541 -0.548 -0.062 0.55 0.97 0.83 0.85 0.24 0.59 0.73 0.54 0.59 ... % disp x(6:14)    
+   0.60 0.64 0.75 0.56 0.66 0.95 0.15 0.15 0.34 ... % disp x(15:23)    
+   0.55 0.59 0.29 0.04 0.35 0.21 0.84 0.14 0.19 0.76 0.07 0.16 ... % hb x(24:35) 
+   6.72 ... % comb x(36)            
+   0.48 ]; % cav x(37)            
+x0_9 =[0.453 -48.813 -0.541 -0.548 -0.062 0.50 0.53 0.31 0.13 0.35 0.51 0.70 0.60 0.56 ... % disp x(6:14)    
+   0.41 0.34 0.55 0.88 0.78 0.35 0.13 0.99 0.54 ... % disp x(15:23)    
+   0.14 0.20 0.66 0.74 0.94 0.50 0.70 0.51 0.91 0.70 0.34 0.25 ... % hb x(24:35) 
+   5.92 ... % comb x(36)            
+   0.82 ]; % cav x(37)            
+x0_10 =[0.453 -48.813 -0.541 -0.548 -0.062 0.70 0.27 0.28 0.85 0.43 0.84 0.42 0.39 0.33 ... % disp x(6:14)    
+   0.50 1.00 0.31 0.97 0.73 0.58 0.40 0.47 0.12 ... % disp x(15:23)    
+   0.66 0.47 0.45 0.30 0.19 0.14 0.62 0.32 0.38 0.55 0.36 0.64 ... % hb x(24:35) 
+   8.24 ... % comb x(36)            
+   0.20 ]; % cav x(37)            
+x0_11 =[0.453 -48.813 -0.541 -0.548 -0.062 0.22 0.23 0.73 0.04 0.03 0.48 0.30 0.36 0.18 ... % disp x(6:14)    
+   0.63 0.43 0.68 0.91 0.82 0.89 0.85 0.48 0.03 ... % disp x(15:23)    
+   0.99 0.89 0.48 0.91 0.01 0.04 0.20 0.85 0.06 0.63 0.74 0.66 ... % hb x(24:35) 
+   4.57 ... % comb x(36)            
+   0.71 ]; % cav x(37)            
+x0_12 =[0.453 -48.813 -0.541 -0.548 -0.062 0.80 0.89 0.04 0.11 0.79 0.27 0.65 0.51 0.92 ... % disp x(6:14)    
+   0.12 0.99 0.17 0.22 0.04 0.48 0.10 0.68 0.74 ... % disp x(15:23)    
+   0.84 0.13 0.09 0.66 1.00 0.58 0.34 0.33 0.73 0.83 0.09 0.66 ... % hb x(24:35) 
+   3.36 ... % comb x(36)            
+   0.17 ]; % cav x(37)            
+
+x0=x0_1;
+
 
 x0 = [0.5 -60 -0.5   -0.5*tanh(- -0.5)     0 ... slic-es x(1:5)
       64170 23743 37793 42479 42223 8157 0 106000 5161 ...  % disp x(6:23)
@@ -141,13 +217,13 @@ ub = [+2 +200 +100 +20  +0.1 ...
       100000 100000 100000 100000 100000 100000 100000 200000 100000 ...
       100000 100000 100000 100000 100000 100000 100000 100000 1 ...
       2 2 2 2 2 2 2 2 2 2 2 2 ...
-      40 ...
-      2];
+      20 ...
+      3];
 lb = [-2 -200 -100 -20  -0.1 ...
       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ...
       0 0 0 0 0 0 0 0 0 0 0 0 ...
       1 ...
-      0.5];
+      0.25];
 
 options = optimoptions('lsqnonlin','MaxIter',8);
 options = optimoptions(options,'Display', 'iter');
@@ -157,17 +233,29 @@ y = @(x)ObjectiveFromBEMCosmo(x);
 [err,calc,ref,es,np,hb,disp,disp_slsl,disp_svsl,disp_svsv,cav,comb]=ObjectiveFromBEMCosmo(x);
 [err0,calc0,ref0,es0,np0,hb0,disp0,disp_slsl0,disp_svsl0,disp_svsv0,cav0,comb0]=ObjectiveFromBEMCosmo(x0);
 [~,id]=ismember(training_set,mol_list);
+
 disp_mob = allData.disp_mobley(id); 
 cav_mob = allData.cav_mobley(id); 
 np_mob = allData.np_mobley(id); 
 es_mob = allData.es_mobley(id); 
 np_SLIC = allData.np_SLIC(id); 
 es_SLIC= allData.es_SLIC(id);
+
 rmse = rms(calc-ref);
-save('OptCosmoBondii.mat','x','training_set','mol_list','rmse','ref','calc','es','np','hb','disp',...
+rmse_disp = rms(disp-disp_mob)
+rmse_cav = rms(cav-cav_mob)
+rmse_np = rms(np-np_mob)
+rmse_es =  rms(es-es_mob)
+
+
+save('OptCosmoBondii_1.mat','x','training_set','mol_list',...
+     'rmse','ref','calc','es','np','hb','disp',...
      'disp_slsl','disp_svsl','disp_svsv','comb','cav',...
      'disp_mob','cav_mob','np_mob','es_mob','np_SLIC',...
      'x0','calc0','es0','np0','hb0','disp0', 'disp_slsl0','disp_svsl0',...
-     'disp_svsv0','comb0','cav0','epsOut');
+     'disp_svsv0','comb0','cav0','epsOut',
+     'rmse_disp','rmse_cav','rmse_np','rmse_es');
+
+
 
 
--- mobley/slic-COSMO/runCosmo_Bondii.m
+++ mobley/slic-COSMO/runCosmo_Bondii.m
@@ -19,11 +19,17 @@ logfileName = 'water.out';
 epsOut = 78.36;
 allData = readtable('all_Bondii_data.csv'); 
 
-ParamWatInfo = load('OptCosmoBondii.mat');
+
+ParamWatInfo = load('OptCosmoBondii_1.mat');
 x = ParamWatInfo.x;
 training_set = ParamWatInfo.training_set;
 mol_list = allData.solute;
 dG_list = allData.dG_expt;
+disp_mob = allData.disp_mobley; 
+cav_mob = allData.cav_mobley; 
+np_mob = allData.np_mobley; 
+np_SLIC = allData.np_SLIC; 
+dG_es_best = allData.best_elec;
 
 saveMemory = 1;
 writeLogfile = 1;
@@ -36,9 +42,9 @@ staticpotential = 0.0; % this only affects charged molecules;
 
 kappa = 0.0;  % should be zero, meaning non-ionic solutions!
 UsefulConstants = struct('epsIn',epsIn,'epsOut',epsOut,'kappa', ...
-			 kappa,'conv_factor',conv_factor,...
-			 'staticpotential',staticpotential);
-%COSMO H-bond atom types
+       kappa,'conv_factor',conv_factor,...
+       'staticpotential',staticpotential);
+
 allHbondTypes = {'n_amine','n_amide','n_nitro',...
                  'n_other','o_carbonyl','o_ester',...
                  'o_nitro','o_hydroxyl','fluorine',...
@@ -47,7 +53,7 @@ allHbondTypes = {'n_amine','n_amide','n_nitro',...
 % here we define the actual params for the NLBC test
 solventAreas = allData{495,9:79};
 solventATypes = allData{495,80:144};
-solventHbData = allData{495,145:end};
+solventHbData = allData{495,145:158};
 solventVdWA = allData{495,11};
 solventVdWV = allData{495,12};
 temperature = 24.85 + KelvinOffset;
@@ -61,9 +67,9 @@ for i=1:length(mol_list)
   chargeDist{i} = pqrData.q;%chargeDistribution;
   soluteAtomAreas{i} = allData{i,9:79};
   soluteAtomTypes{i} = {allData{i,80:144}};
-  soluteHbondData{i} = allData{i,145:end};
   solute_VdWA{i} = allData{i,11};
   solute_VdWV{i} = allData{i,12};
+  soluteHbondData{i} = allData{i,145:158};
   solventAtomAreas{i} = solventAreas;
   solventAtomTypes{i} = {solventATypes};
   solventHbondData{i} = solventHbData;
@@ -71,10 +77,10 @@ for i=1:length(mol_list)
   solvent_VdWV{i} = solventVdWV;
   atom_vols{i} = allData{i,14};
   temp{i} = temperature;
-  referenceData{i} = dG_list(i);
+  referenceData{i} = np_mob(i);
   newHB{i}=0;
   chdir(curdir);
-  addProblemCosmo(mol_list{i},pqrAll{i},srfFile{i},chargeDist{i},referenceData{i},...
+  addProblemCosmoNpHB(mol_list{i},chargeDist{i},referenceData{i},...
                   soluteAtomAreas{i},soluteAtomTypes{i},soluteHbondData{i},...
                   solute_VdWV{i},solute_VdWA{i},...
                   solventAtomAreas{i},solventAtomTypes{i},solventHbondData{i},...
@@ -82,21 +88,21 @@ for i=1:length(mol_list)
                   atom_vols{i},temp{i},newHB{i});
 
 
-
-end
 disp_mob = allData.disp_mobley; 
 cav_mob = allData.cav_mobley; 
 np_mob = allData.np_mobley; 
 es_mob = allData.es_mobley; 
 np_SLIC = allData.np_SLIC; 
 es_SLIC= allData.es_SLIC;
-% xNoIonsInParam = [  1.8570  -75.6855   -2.9884    8.7302    0.0113    0.4098];
-% xIonsInParam   = [  0.5479 -97.8176 -1.0219 -9.7033 0.0049 2.4240];
-% xIonsAndMDStat = [    0.4004  -44.3595   -1.1232   -0.7356    0.0139    2.5034];
-% xIonsAnd8Iter  = [   0.6260 -112.0291   -0.9296  -12.1084    0.0037    2.7546];
-% 
-% x = xIonsAnd8Iter;
+
 [err,calc,ref,es,np,hb,disp,disp_slsl,disp_svsl,disp_svsv,cav,comb]=ObjectiveFromBEMCosmo(x);
-save('RunCosmoBondii.mat','mol_list','training_set','err','calc','ref','es','np','hb',...
+rmse = rms(dG_es_best+calc-dG_list);
+rmse_np = rms(np  - np_mob);
+rmse_es = rms(es  - es_mob);
+rmse_disp = rms(disp - disp_mob);
+rmse_cav = rms(cav - cav_mob);
+
+save('RunCosmoBondii_1.mat','mol_list','training_set','err','calc','ref','es','np','hb',...
     'disp','disp_slsl','disp_svsl','disp_svsv','cav','comb',...
-     'disp_mob','cav_mob','np_mob','es_mob','np_SLIC','es_SLIC');
+     'disp_mob','cav_mob','np_mob','es_mob','np_SLIC','es_SLIC',...
+     'rmse','rmse_np','rmse_es','rmse_disp','rmse_cav'););
